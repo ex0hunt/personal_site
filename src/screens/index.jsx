24201931adbfc78f129876803baa63850ed5cb09
Dmitry K <ex0hunt@yandex.ru>
2020-06-21 21:20:16 +0300

    NOISSUE: stylish footer

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Switch, Route, Link} from 'react-router-dom'
-import {Navbar, Nav, Row, Col, Button} from 'react-bootstrap'
+import {Row, Col} from 'react-bootstrap'
 
 import WhoAmIScreen from "./about.jsx";
 import BlogScreen from "./blog.jsx";
 import EntsScreen from "./ents.jsx";
 import ServicesScreen from "./services.jsx";
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter'
+import {faGithub} from '@fortawesome/free-brands-svg-icons/faGithub'
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons/faLinkedin'
+
+library.add(faTwitter, faGithub, faLinkedin);
+
 
 function SelectedBackgroundStyle(currentPath, itemPath) {
     if (currentPath.startsWith(itemPath)) {
@@ -27,7 +36,7 @@ function IndexNavBar(props) {
     let currentPath = window.location.pathname;
 
     return (
-        <ul className={'nav-head'}>
+        <ul >
             <li className={'nav-head-item'} style={SelectedBackgroundStyle(currentPath, "/whoami/")}>
                 <Link className={'nav-head-link'} to={"/whoami/"}
                       style={SelectedFontStyle(currentPath, "/whoami/")}>About me</Link>
@@ -44,7 +53,7 @@ function IndexNavBar(props) {
             </li>
             <li className={'nav-head-item'} style={SelectedBackgroundStyle(currentPath, "/services/")}>
                 <Link className={'nav-head-link'} to={"/services/"}
-                      style={SelectedFontStyle(currentPath, "/services/")}>My Services</Link>
+                      style={SelectedFontStyle(currentPath, "/services/")}>Services</Link>
 
             </li>
         </ul>
@@ -64,7 +73,7 @@ export default class IndexScreen extends React.Component {
                     <h1 className="text-center branding">Ex0</h1>
                      </Col>
                 </Row>
-                <Row className="show-grid">
+                <Row className="show-grid" className={'nav-head'}>
                     <Col md={12}  className="text-center">
                         <IndexNavBar/>
                     </Col>
@@ -88,9 +97,34 @@ export default class IndexScreen extends React.Component {
             </div>
         )
     }
+    fooBar() {
+        return(
+            <Row className="show-grid">
+                <Col md={12}  className="text-center">
+                    <ul className={'nav-foo'}>
+                        <li className={'nav-foo-item'}>
+                            <a href="https://github.com/ex0hunt" className={'nav-foo-link'}><FontAwesomeIcon icon={["fab", "github"]}/> GitHub</a>
+                        </li>
+                        <li className={'nav-foo-item'}>
+                            <a href="https://www.linkedin.com/in/dmitry-katargin-44a36250" className={'nav-foo-link'}><FontAwesomeIcon icon={["fab", "linkedin"]}/> LinkedIn</a>
+                        </li>
+                        <li className={'nav-foo-item'}>
+                            <a href="https://twitter.com/ex0hunt" className={'nav-foo-link'}><FontAwesomeIcon icon={["fab", "twitter"]}/> Twitter</a>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+        )
+    }
 
     render() {
         const content = this.navBar()
-        return (content)
+        const footer = this.fooBar()
+        return (
+            <Fragment>
+                {content}
+                {footer}
+            </Fragment>
+        )
     }
 }
