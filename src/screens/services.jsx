148c14b0ee0799be19b18a1d5322ae23deb2d68e
Dmitry K <ex0hunt@yandex.ru>
2020-06-27 06:03:24 +0300

    NOISSUE: add services;

--- src/screens/services.jsx
+++ src/screens/services.jsx
@@ -1,20 +1,22 @@
-import React, {Fragment} from "react";
-import {Col, Image, Row} from "react-bootstrap";
+import React from "react";
+import {Table} from "react-bootstrap";
 
 
 export default class ServicesScreen extends React.Component {
     render() {
-        return (<Fragment>
-            <Row>
-                <Col md={12} className={'warning'}>
-                    <h1>⚠ Under construction ⚠</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={12} className={'warning'}>
-                    <Image src={"https://exo.icu/public/media/warn.gif"}></Image>
-                </Col>
-            </Row>
-        </Fragment>)
+        return (<div className={"container"}>
+                <Table striped responsive="sm" className={'services-table'}>
+                    <tbody>
+                    <tr>
+                        <td><h4><b><a href={'https://pst.exo.icu/'} target={"_blank"}>PST</a></b></h4></td>
+                        <td> <h4>simple paste service</h4></td>
+                    </tr>
+                    <tr>
+                        <td><h4><b><a href={'https://q3.exo.icu/'} target={"_blank"}>Q3</a></b></h4></td>
+                        <td><h4>Quake3 server</h4></td>
+                    </tr>
+                    </tbody>
+                </Table>
+        </div>)
     }
 }
